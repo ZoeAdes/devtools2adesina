512141c533f6f5d1acc8687278cc385a687a4574
Zoe Adesina <zoeadesina@Zoes-MacBook-Pro.local>
2017-03-01 14:29:47 -0500

    removed testconnection.php file after database connection issue was resolved

--- testconnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$test Connection = mysql_connect('localhost', 'root', 'password');
-if (!$testConnection) {
-die('Error: ' . mysql_error());
-}
-echo 'Database connection working!';
-mysql_close($testConnection);
-?>
